08eeca0fd79c6961b943e3831751732dc3fb941b
Doorknob12 <106000739+Doorknob12@users.noreply.github.com>
2022-11-09 08:35:43 -0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,13 @@
+function init() {
+  //add your javascrip between these two lines of code
+  //window.addEventListener("click",myfunction);
+  function myTaskFunction() {
+    var inputval = document.getElementById('entryinput');
+    var outputval = document.getElementById('textoutput');
+    alert("Sam Hoppenworth: " + inputval.value);
+    outputval.innerHTML = inputval.value;
+  }
+  var clickbutton = document.getElementById('entrybutton');
+  clickbutton.addEventListener('click', myTaskFunction);
+}
+window.addEventListener('load', init);
